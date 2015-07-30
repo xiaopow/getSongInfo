331bbbc0baa5a397c4330da71b9360d6657372af
Xiaopow <xiaoyang88@gmail.com>
2015-07-30 16:25:17 +0800

    Initial Commit

---
+++ musicControllerInfoGrabber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicControllerInfoGrabber
+//
+//  Created by xiao yang on 30/7/15.
+//  Copyright (c) 2015 xiaopow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
